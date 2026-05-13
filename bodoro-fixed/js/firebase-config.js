6523ac6255818413e5482b8ca8f5874b69f4e0c8
coucoudiabate-hub <coucou.diabate@gmail.com>
2026-05-13 11:22:55 +0000

    Delete bodoro-fixed directory

--- bodoro-fixed/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBkoefGJhiWFh5eHEwA4gnlsU5mOUoIM7I",
-  authDomain: "bodoro-menu.firebaseapp.com",
-  projectId: "bodoro-menu",
-  storageBucket: "bodoro-menu.firebasestorage.app",
-  messagingSenderId: "766107406239",
-  appId: "1:766107406239:web:8d768e00dfd8f7885577aa"
-};
